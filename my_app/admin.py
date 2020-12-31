82c18a7971ae190ef249cb7c5ee91b4bf965eb8e
MNEIMOH <juniornkiangmatiah>
2020-12-31 16:42:06 +0100

    This is are the files for the starting of our craiglist clone application!

---
+++ my_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . import models
+#from .models import Search
+
+# Register your models here.
+
+admin.site.register(models.Search)
+#admin.site.register(Search)
